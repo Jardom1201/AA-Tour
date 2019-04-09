e4f46d9b85d872e9507c41e586a92552fcbbea5a
basriyal <basriyal30@gmail.com>
2019-04-09 14:43:38 +0700

    login done

---
+++ app/src/main/java/com/example/aatour/login2.java
@@ -0,0 +1,26 @@
+package com.example.aatour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        Button signin = findViewById(R.id.signin);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(login2.this, HomePage.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
